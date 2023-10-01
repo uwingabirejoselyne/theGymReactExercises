d669b06a1965a98af4863d4fb454338389b69365
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-10-01 09:39:29 +0200

    indeex

---
+++ src/Components/CapturingUserClicks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CapturingUserClicks = () => {
+  return (
+    <div className="flex justify-center items-center">
+        <h1 className='text-red-600'>Hello world</h1>
+       <div className="w-96 h-96 bg-red-600 flex justify-center items-center">
+
+      </div>
+    </div>
+  )
+}
+
+export default CapturingUserClicks
