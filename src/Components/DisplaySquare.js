cfcb913cbbcac7de3f431775046d5c188ed12899
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-10-01 08:56:58 +0200

    ft: display square that display text

---
+++ src/Components/DisplaySquare.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DisplaySquare = () => {
+  return (
+    <div className=' flex justify-center items-center  pt-32'>
+        <div className='h-96 w-96 bg-[#fec800]  flex justify-center items-center'>
+
+            <p>Hello, World!</p>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default DisplaySquare
