15960a21a1a542473c7ac80bfcaccfebb346702b
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-10-02 15:20:25 +0200

    Working with an Api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { async } from 'q';
 import './App.css';
+import UserCards from './Components/UserCards'
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App =() => {
+  const [users, setUsers] = useState([])
+  useEffect(() =>{
+    const fetchUsers = async ()=>{
+      const response = await fetch('https://random-data-api.com/api/users/random_user?size=10')
+      const data  = await response.json()
+      console.log(data);
+      setUsers(data)
+    }
+    fetchUsers()
+
+  },[])
   return (
-    <div className="App">
-      fasfsadf
+    <div className="grid grid-cols-2 justify-center">
+      {users.map((user) => (
+        <UserCards  user={user} />
+      ))}
     </div>
   );
 }
