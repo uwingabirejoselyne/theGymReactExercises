4cc492dbe38628cceac9280c4a11bfb283673172
Joselyne uwingabire <uwingajoselyne@gmail.com>
2023-10-13 19:13:27 +0200

    Store data in localstorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,84 @@
-import './App.css';
+import React from "react"
+import Split from "react-split"
+import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
+import Sidebar from "./Components/Sidebar"
+import Editor from "./Components/Editor"
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default function App() {
+
+    const storedNotes = localStorage.getItem("notes");
+    const initialNotes = storedNotes ? JSON.parse(storedNotes) : [];
+    
+    const [notes, setNotes] = React.useState(initialNotes);
+    const [currentNoteId, setCurrentNoteId] = React.useState(
+        (initialNotes[0] && initialNotes[0].id) || ""
+    );
+    
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+    
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+    }
+    
+    function updateNote(text) {
+        setNotes(oldNotes => oldNotes.map(oldNote => {
+            return oldNote.id === currentNoteId
+                ? { ...oldNote, body: text }
+                : oldNote
+        }))
+    }
+    
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+    
+    return (
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+            
+        }
+        </main>
+    )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+const root = createRoot(document.getElementById('root'));
 root.render(<App />);
